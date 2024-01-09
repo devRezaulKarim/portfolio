56a77c75e5c9aa224da57c4ec7fcdbe866ec21eb
devRezaulKarim <dev.rezaulkarim@gmail.com>
2024-01-09 10:21:35 +0600

    Navbar design done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ import { IoIosPaper } from "react-icons/io";
 
 export default function Navbar() {
   return (
-    <div className="bg-[var(--color-secondary)] text-[var(--color-light)] fixed top-0 right-0 w-40  h-full">
+    <div className="bg-[var(--color-secondary)] text-[var(--color-light)] fixed top-0 right-0 w-40  h-full shadow-[inset_10px_0_10px_-10px_var(--color-accent)]	">
       <div className="">
         <div className="text-center py-4">
           <h1 className="uppercase text-3xl font-black text-[var(--color-light)]">
