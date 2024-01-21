c48e334f564b2bc81cb3c1269c7e636fde8232b0
devRezaulKarim <dev.rezaulkarim@gmail.com>
2024-01-21 11:05:37 +0600

    hero image scr changed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -31,7 +31,7 @@ export default function Home() {
     <div className="">
       <div className="lg:flex flex-row-reverse items-center ">
         <div className="heroRight  ">
-          <img className="w-full" src="/Programming1.png" alt="" />
+          <img className="w-full" src="/Programming.png" alt="" />
         </div>
         <div className="heroLeft  flex flex-col gap-2">
           <h2 className="text-2xl font-semibold mb-4">Hey there, </h2>
