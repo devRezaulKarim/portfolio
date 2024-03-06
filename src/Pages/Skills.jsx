c1b02b04afe83d4da6f4e8f46aead1249f3184d3
devRezaulKarim <dev.rezaulkarim@gmail.com>
2024-03-06 16:19:55 +0600

    FEAT: Add redux as a skill set

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -10,12 +10,12 @@ export default function Skills() {
     <div className="skills relative">
       <h1 className="sectionTitle skillsTitle">Skills</h1>
       <div className=" w-full h-80 relative ">
-        {skillsLogo.slice(0, 7).map((language, i) => (
+        {skillsLogo.slice(0, 8).map((language, i) => (
           <SkillsBall key={i} language={language} />
         ))}
       </div>
       <div className="lg:grid grid-cols-2 gap-x-8">
-        {skillsText.slice(0, 7).map((skill, i) => (
+        {skillsText.slice(0, 8).map((skill, i) => (
           <Skill key={i} skill={skill} />
         ))}
       </div>
