39d186afa8a6bb58b5dba018fed352d81e461a2f
devRezaulKarim <dev.rezaulkarim@gmail.com>
2025-07-25 21:20:34 +0600

    FEAT: Add hover animation to the tool icon

--- src/components/shared/tools-card.tsx
+++ src/components/shared/tools-card.tsx
@@ -1,17 +1,34 @@
 import type { ToolsType } from "../../lib/custom-types";
+import { cn } from "../../lib/utls";
 
 interface ToolsCardProps extends ToolsType {
   className?: string;
 }
 
 export const ToolsCard = ({ toolName, toolIcon: ToolIcon }: ToolsCardProps) => {
+  const getRotateClass = () => {
+    const classes = [
+      "group-hover:rotate-x-360",
+      "group-hover:rotate-y-360",
+      "group-hover:rotate-z-360",
+    ];
+    return classes[Math.floor(Math.random() * 3)];
+  };
+
+  const rotateClass = getRotateClass();
+
   return (
     <div
-      className="grid !aspect-square w-full place-items-center rounded-lg p-3"
+      className="group grid !aspect-square w-full cursor-default place-items-center rounded-lg p-3"
       style={{ boxShadow: "0 0 10px 0 #ccc" }}
     >
-      <div className="flex flex-col items-center gap-2">
-        <ToolIcon className="w-8 sm:w-10 md:w-14" />
+      <div className={`flex flex-col items-center gap-2`}>
+        <ToolIcon
+          className={cn(
+            "inline-block w-8 duration-[1.5s] sm:w-10 md:w-14",
+            rotateClass,
+          )}
+        />
         <p className="text-base sm:text-lg lg:text-xl">{toolName}</p>
       </div>
     </div>
